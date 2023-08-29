68d6db955c57a85352ebb880a5cc70dc31108cb3
ChaconFront <138753387+ChaconFront@users.noreply.github.com>
2023-08-29 07:31:18 -0400

    Update index_dom.js

--- index_dom.js
+++ index_dom.js
@@ -1,23 +1,12 @@
-import countdown from "./dom/cuenta_regresiva.js";
-import hamburgerMenu  from "./dom/menu_hamburgues.js";
-import {digitalClock,alarma} from './dom/reloj.js'
-import { moveBall } from "./dom/teclado.js";
+
+import {digitalClock} from './dom/reloj.js'
+
 
 const d=document;
 
 //este evento nos va a permitir que nuestra funcion se ejecute a la carga del documento.
-d.addEventListener("DOMContentLoaded",(e)=>{
-    hamburgerMenu(".panel-btn",".panel",".menu a");
-    digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
-    alarma("assets/alarma.mp3","#activar-alarma","#desactivar-alarma");
-    countdown("countdown","May23,2021 03:23:19","Feliz cumoleaños🤓")
+d.addEventListener("DOMContentLoaded",(e)=>{  
+    digitalClock("#reloj","#activar-reloj","#desactivar-reloj");    
 });
-
-//evento del teclado.
- d.addEventListener("keydown",e=>{
-    moveBall(e,".ball",".stage");
- })
-
-
 
  
